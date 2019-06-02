4c5b19644a3f48e984a48beccb5408b0e6db47d8
jesuscovam <46403187+jesuscovam@users.noreply.github.com>
2019-06-02 18:33:20 -0500

    Add files via upload

---
+++ actividades.js
@@ -0,0 +1,44 @@
+db.actividades2.insertMany(
+    [
+        {
+            nombre: 'Ruta de Cenotes',
+            tipo: 'naturaleza',
+            precio: 40
+        },
+        {
+            nombre: 'Xel-Ha',
+            tipo: 'naturaleza',
+            precio: 60
+        },
+        {
+            nombre: 'Coco Bongo',
+            tipo: 'diversion',
+            precio: 80
+        },
+        {
+            nombre: 'Mandalas',
+            tipo: 'diversion',
+            precio: 50
+        },
+        {
+            nombre: 'Xcaret',
+            tipo: 'experiencia',
+            precio: 110
+        },
+        {
+            nombre: 'Río Secreto',
+            tipo: 'experiencia',
+            precio: 70
+        },
+        {
+            nombre: 'Museo Frida Kahlo',
+            tipo: 'cultura',
+            precio: 30
+        },
+        {
+            nombre: 'Chichen Itza',
+            tipo: 'cultura',
+            precio: 50
+        }
+    ]
+);
